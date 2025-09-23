04a846e855aaab8abd2b1ad3df8391f8898a4c5a
vanhellsing102 <vvanhellsing30@gmail.com>
2025-09-22 23:21:12 -0600

    edit

--- src/Layout/ProfileRoot.jsx
+++ src/Layout/ProfileRoot.jsx
@@ -22,7 +22,7 @@ const ProfileRoot = () => {
     
     // console.log(pathname);
     return (
-        <div className='bg-gray-100 py-6 md:py-8 lg:py-10 px-3 md:px-16 lg:px-20'>
+        <div className='bg-gray-100 min-h-screen py-6 md:py-8 lg:py-10 px-3 md:px-16 lg:px-20'>
             <div className='mb-6 md:mb-8 lg:mb-10'>
                 <h1 className='text-2xl md:text-3xl lg:text-4xl text-red-950'>Account Settings</h1>
                 <p className='text-sm text-gray-600'>Manage your accout preferences and information</p>
