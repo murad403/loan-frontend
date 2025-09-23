ba890b36c8eabad16721467e9e21a662173a322e
vanhellsing102 <vvanhellsing30@gmail.com>
2025-09-22 22:54:59 -0600

    api change

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://loan-backend-l3l3.onrender.com",
     withCredentials: true
 })
 const useAxiosPublic = () => {
